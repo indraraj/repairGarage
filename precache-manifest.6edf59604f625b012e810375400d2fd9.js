32c520f94c7e7cb94167224b222e1315ac03a2ba
Indra Shukla <ishukla@localhost.localdomain>
2020-05-19 00:15:34 +0530

    Updates

---
+++ precache-manifest.6edf59604f625b012e810375400d2fd9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e5f45498e3dca276e84c23cfd4f55e",
+    "url": "/repairGarage/index.html"
+  },
+  {
+    "revision": "213094222e22c89e1b1a",
+    "url": "/repairGarage/static/css/main.26f558ea.chunk.css"
+  },
+  {
+    "revision": "d8d3872c00eea24cce57",
+    "url": "/repairGarage/static/js/2.fe8b2986.chunk.js"
+  },
+  {
+    "revision": "bc23c04eec3c55de9f1e7db1db146add",
+    "url": "/repairGarage/static/js/2.fe8b2986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "213094222e22c89e1b1a",
+    "url": "/repairGarage/static/js/main.824e8bae.chunk.js"
+  },
+  {
+    "revision": "169e79b46b195197e028",
+    "url": "/repairGarage/static/js/runtime-main.fb4b1b5f.js"
+  },
+  {
+    "revision": "08105730fb55b20fd8603f3946fc78b8",
+    "url": "/repairGarage/static/media/background1.08105730.jpg"
+  },
+  {
+    "revision": "285ae49bbdd05c621e0f78c5c877685f",
+    "url": "/repairGarage/static/media/baee.285ae49b.jpeg"
+  },
+  {
+    "revision": "572fe38894e4fadf075217a819fc9a85",
+    "url": "/repairGarage/static/media/google.572fe388.png"
+  },
+  {
+    "revision": "3b47ecc952bb35d8831ae4ccecbd9dc3",
+    "url": "/repairGarage/static/media/indra.3b47ecc9.jpg"
+  },
+  {
+    "revision": "4d7bdbce2a932586081c91db1b079a59",
+    "url": "/repairGarage/static/media/logo.4d7bdbce.PNG"
+  },
+  {
+    "revision": "8d86598f7521e9b4309fb90cf07bc627",
+    "url": "/repairGarage/static/media/riya.8d86598f.jpeg"
+  }
+]);
